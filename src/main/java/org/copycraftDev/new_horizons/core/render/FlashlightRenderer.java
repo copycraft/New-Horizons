7eefa3634351b23478f98f60604404d45033c959
HyperNova <reczek.thomas@gmail.com>
2025-03-11 21:37:35 +0000

    FINISHED THE FLASHLIGHT! DO NOT TOUCH FlashlightRenderer.java!

--- src/main/java/org/copycraftDev/new_horizons/core/render/FlashlightRenderer.java
+++ src/main/java/org/copycraftDev/new_horizons/core/render/FlashlightRenderer.java
@@ -14,29 +14,40 @@ public class FlashlightRenderer extends AreaLight {
     private boolean isOn = false;
     private Vec3d lastPosition = new Vec3d(0, 0, 0);
     private Quaternionf lastRotation = new Quaternionf();
-    private static final float SMOOTH_FACTOR = 0.6f;
+    private static final float SMOOTH_FACTOR = 0.2f;
+    private final AreaLight areaLight2 = new AreaLight();
 
     public FlashlightRenderer() {
         super();
-        this.setDistance(20);
-        this.setSize(1.5, 1.5);
-        this.setColor(1.0f, 0.9f, 0.6f);
-        this.setBrightness(4.0f);
+        this.setDistance(50.0f);
+        this.setSize(1.0, 1.0);
+        this.setColor(1.0f, 1.0f, 0.6470588235f);
+        this.setBrightness(1.5f);
+
+        // Configure secondary area light
+        areaLight2.setDistance(50f);
+        areaLight2.setSize(0.15, 0.15);
+        areaLight2.setBrightness(2.0f);
+        areaLight2.setAngle(0.25f);
+        areaLight2.setColor(1.0f, 1.0f, 0.9411764706f);
     }
 
     public void toggle() {
         isOn = !isOn;
         if (isOn) {
-            this.setDistance(13.0f);
-            this.setSize(0.8, 0.8);
-            this.setColor(1.0f, 0.9f, 0.6f);
-            this.setBrightness(4.0f);
-            this.setAngle((float) Math.toRadians(40));
+            this.setDistance(50.0f);
+            this.setSize(1.0, 1.0);
+            this.setColor(1.0f, 1.0f, 0.6470588235f);
+            this.setBrightness(1.5f);
+            this.setAngle((float) Math.toRadians(35));
+
             VeilRenderSystem.renderer().getLightRenderer().addLight(this);
+            VeilRenderSystem.renderer().getLightRenderer().addLight(areaLight2);
         } else {
             VeilRenderSystem.renderer().getLightRenderer().removeLight(this);
-            this.setDistance(0);
-            this.setSize(0.0, 0.0);
+            VeilRenderSystem.renderer().getLightRenderer().removeLight(areaLight2);
+            this.setDistance(50);
+            this.setSize(0.2, 0.2);
         }
     }
 
@@ -64,7 +75,12 @@ public class FlashlightRenderer extends AreaLight {
         Vector3f forward = new Vector3f(0, 0, 1).rotate(lastRotation);
         Vector3f up = new Vector3f(0, 1, 0).rotate(lastRotation);
 
-        this.setPosition(lastPosition.x, lastPosition.y, lastPosition.z);
-        this.setOrientation(new Quaternionf().lookAlong(forward, up));
+        Quaternionf orientation = new Quaternionf().lookAlong(forward, up);
+
+        this.setPosition(lastPosition.x, lastPosition.y + 0.75, lastPosition.z);
+        this.setOrientation(orientation);
+
+        areaLight2.setPosition(lastPosition.x, lastPosition.y, lastPosition.z);
+        areaLight2.setOrientation(orientation);
     }
 }
